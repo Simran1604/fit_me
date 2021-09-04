190e55ad4d05646d7f562a32f5e0961010197bbf
Simran1604 <simran251601@gmail.com>
2021-09-04 14:47:10 +0530

    basics done

--- lib/database.dart
+++ lib/database.dart
@@ -5,48 +5,59 @@ class databasebase{
   databasebase({required this.uid});
   
   //firestore instance
-  final CollectionReference db=FirebaseFirestore.instance.collection('userdata');
+  
 
   Future createNewuser(String name,String email, String password) async
   {
-    await db.doc(uid).set({
-      'name':name,
-      'email':email,
-      });
+    
   }
+
+   updateName(String name)
+  {
+    final CollectionReference db=FirebaseFirestore.instance.collection('Name');
+     db.doc(uid).set({'name':name});
+  }
+
 
    updateBMI(double bmi)
   {
+    final CollectionReference db=FirebaseFirestore.instance.collection('BMI');
      db.doc(uid).set({'BMI':bmi});
   }
 
    updateAge(double age)
   {
+    final CollectionReference db=FirebaseFirestore.instance.collection('Age');
      db.doc(uid).set({'age':age});
   }
 
     updateCal(int calorie)
   {
+    final CollectionReference db=FirebaseFirestore.instance.collection('calorie');
      db.doc(uid).set({'calorie':calorie});
   }
 
     updatefat(double fat)
   {
+    final CollectionReference db=FirebaseFirestore.instance.collection('Fat%');
      db.doc(uid).set({'fat':fat});
   }
 
     updateGender(String gender)
   {
+    final CollectionReference db=FirebaseFirestore.instance.collection('Gender');
      db.doc(uid).set({'gender':gender});
   }
 
     updateHeight(double height)
   {
+    final CollectionReference db=FirebaseFirestore.instance.collection('Height');
      db.doc(uid).set({'height':height});
   }
 
     updateWeight(double weight)
   {
+    final CollectionReference db=FirebaseFirestore.instance.collection('Weight');
      db.doc(uid).set({'weight':weight});
   }
 }
